a5b4ccf939a891c0b6246760570cc073e0628127
OnofreAraujo <onofresimiao.arauj@gmail.com>
2020-06-30 00:21:46 -0300

    v5

--- protected.php
+++ protected.php
@@ -4,7 +4,7 @@
 		function proteger(){
 			if(!isset($_SESSION)) session_start();
 			if(!isset($_SESSION['login'])){
-				header("Location: /ead/home.php");
+				header("Location: /home.php");
 			}
 		}
 	}
@@ -12,14 +12,14 @@
 	if(!function_exists("anti_adm")){
 		function anti_adm(){
 			if(!isset($_SESSION['cargo']) && isset($_SESSION['admin']) != "PROFESSOR"){
-				header("Location: home.php");
+				header("Location: /home.php");
 			}
 		}
 	}
 
 	if(!function_exists("pagina_off")){
 		function pagina_off(){
-			header("Location: home.php");
+			header("Location: /home.php");
 		}
 	}
 
